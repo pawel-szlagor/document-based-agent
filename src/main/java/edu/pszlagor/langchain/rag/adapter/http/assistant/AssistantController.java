c32d8dbfd396a960d32a75cf6a3e686c9c28bb9f
pawel-szlagor <pawel.szlagor@knowde.com>
2024-06-09 22:21:42 +0200

    Chat
    - added OpenAPI support

--- src/main/java/edu/pszlagor/langchain/rag/adapter/http/assistant/AssistantController.java
+++ src/main/java/edu/pszlagor/langchain/rag/adapter/http/assistant/AssistantController.java
@@ -2,6 +2,11 @@ package edu.pszlagor.langchain.rag.adapter.http.assistant;
 
 import edu.pszlagor.langchain.rag.application.assistant.AssistantService;
 import edu.pszlagor.langchain.rag.application.assistant.DocumentScopedQuestion;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +21,13 @@ class AssistantController {
         this.assistantService = assistantService;
     }
 
+    @Operation(summary = "Answers the given question based on the document with given ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully answered the question using information included in the referenced document.",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = String.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid request",
+                    content = @Content)})
     @PostMapping("/chat")
     public String chat(@RequestBody ChatRequest message) {
         return assistantService.chat(new DocumentScopedQuestion(message.documentId(), message.question()));
--- src/main/java/edu/pszlagor/langchain/rag/adapter/http/document/DocumentController.java
+++ src/main/java/edu/pszlagor/langchain/rag/adapter/http/document/DocumentController.java
@@ -3,6 +3,11 @@ package edu.pszlagor.langchain.rag.adapter.http.document;
 import edu.pszlagor.langchain.rag.application.document.DocumentDto;
 import edu.pszlagor.langchain.rag.application.document.DocumentService;
 import edu.pszlagor.langchain.rag.application.document.exception.DocumentLoadingException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +24,13 @@ class DocumentController {
         this.documentService = documentService;
     }
 
+    @Operation(summary = "Uploads a file and stores in Embedding Store")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "File successfully processed and saved under returned ID",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = String.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid file",
+                    content = @Content)})
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(value = "/upload", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     public String uploadFile(@RequestParam("file") MultipartFile file) {
